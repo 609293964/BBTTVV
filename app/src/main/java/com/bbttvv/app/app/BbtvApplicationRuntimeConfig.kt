8f92b52f817ced478ed1d9e03f34750e8d23990a
莫宇海 <609293964@qq.com>
2026-05-07 22:04:53 +0800

    00.8
    008

--- app/src/main/java/com/bbttvv/app/app/BbtvApplicationRuntimeConfig.kt
+++
@@ -1,7 +0,0 @@
-package com.bbttvv.app.app
-
-internal object BbtvApplicationRuntimeConfig {
-    const val TAG: String = "BbtvApplication"
-
-    fun deferredNonCriticalStartupDelayMs(): Long = 600L
-}
--- app/src/main/java/com/bbttvv/app/navigation/NavigationSavedStateKeys.kt
+++
@@ -1,4 +0,0 @@
-package com.bbttvv.app.navigation
-
-internal const val PublisherPreviewNameSavedStateKey = "publisher_preview_name"
-internal const val PublisherPreviewFaceSavedStateKey = "publisher_preview_face"
